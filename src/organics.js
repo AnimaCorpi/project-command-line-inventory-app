0f9d1d99d37142e4289c3d4594018ac3f6fcd82e
Anamaria Torres <anamariat528@icloud.com>
2023-06-11 16:12:36 -0400

    updated data files

--- src/organics.js
+++ src/organics.js
@@ -1,3 +1,8 @@
 const organicsData = require("../data/organicsData.json")
 
-console.log(organicsData)
+//console.log(organicsData) ✅ check to make sure the the dat file was properly exported
+
+
+// example data type
+//  {"itemType": "fruit", "itemName": "apple", "price": 1.25, "quantity": 100},
+
