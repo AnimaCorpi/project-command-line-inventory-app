063933f5864841ce2cf484adde63da4149d398e7
Anamaria Torres <anamariat528@icloud.com>
2023-06-12 00:09:04 -0400

    added new functions

--- src/organics.js
+++ src/organics.js
@@ -6,3 +6,36 @@ const organicsData = require("../data/organicsData.json")
 // example data type
 //  {"itemType": "fruit", "itemName": "apple", "price": 1.25, "quantity": 100},
 
+
+//checks the inventory
+    //checks the quantity of each item
+        //if it is not stock, it will add a new key called "inStock" to the object, setting it to "false"
+            // if not stock, it puts all the out of stock items into a seperate array
+function checkInventory (organicsData){
+    let outOfStock = ["out of stock items:", ]
+    for (let items of organicsData){
+        if (items.quantity === 0) {
+            items["inStock"] = false
+            outOfStock.push(items)
+        }
+    }
+    return outOfStock
+}
+//console.log (checkInventory(organicsData)) ✅ works
+
+function addToInventory (organicsData, type, name, itemPrice, itemQuantity) {
+    let obj = {"itemType": `${type}`, "itemName": `${name}`, "price": `${itemPrice}`, "quantity": `${itemQuantity}`}
+    organicsData.push(obj)
+    return organicsData
+}
+//console.log(addToInventory(organicsData, "candy", "skittles", 0.75, 50)) ✅ works
+function removeFromInventory(organicsData, name) {
+    for (let items = 0; items < organicsData.length; items++) {
+        // const element = organicsData[items];
+        if (items.itemName === `${name}`) {
+            organicsData.splice(items,1)
+        }
+    }
+    return organicsData
+}
+console.log(removeFromInventory(organicsData, "apple"))
